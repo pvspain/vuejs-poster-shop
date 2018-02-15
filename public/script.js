28600f4f20145b96fdf4403a5f38f18dd59b1b1f
Paul Spain <pvspain@gmail.com>
2018-02-15 17:59:51 +1100

    Add button to Products div and bind  Vue methods function addItem to button on-click.

--- public/script.js
+++ public/script.js
@@ -1,5 +1,7 @@
 //jslint white
- 
+
+"use strict"
+
 // Create new Vue instance and anchor in DOM inside div ELement wih id 'app'  (index.html)
 // - use CSS selector syntax to identify anchor point
 
@@ -7,5 +9,11 @@ new Vue({
   el: '#app',
   data: {
     total: 0
+  },
+  methods: {
+    "addItem": function () { 
+      console.log("addItem");
+      this.total += 9.99 
+    }
   } 
 });
