bc34a6298f400df972ebf2f3de19c264cb60117e
Paul Spain <pvspain@gmail.com>
2018-02-15 17:31:46 +1100

    reference elements in data object inside DOM using mustache syntax

--- public/script.js
+++ public/script.js
@@ -1,6 +1,11 @@
+//jslint white
+ 
 // Create new Vue instance and anchor in DOM inside div ELement wih id 'app'  (index.html)
 // - use CSS selector syntax to identify anchor point
 
 new Vue({
-  el: '#app' 
+  el: '#app',
+  data: {
+    total: 0
+  } 
 });
